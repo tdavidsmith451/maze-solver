288bf982fb5efd6ccc4f51b0ba8d378b543177f0
tdavidsmith451 <tdavidsmith@live.com>
2024-10-17 16:27:07 -0600

    added a draw_move method to the celll class

--- cell.py
+++ cell.py
@@ -1,19 +1,5 @@
 from graphics import *
 
-class Point:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-
-class Line:
-    def __init__(self, p1, p2):
-        self.p1 = p1
-        self.p2 = p2
-
-    def draw(self, canvas, fill_color):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
-
 class Cell:
     def __init__(self, window):
         self.has_left_wall = True
@@ -46,3 +32,16 @@ class Cell:
         if self.has_bottom_wall == True:
             bottom_line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(bottom_line, "black")
+
+    def get_center_point(self):
+        center_xcoord = (self._x2 - self._x1) / 2 + self._x1
+        center_ycoord = (self._y2 - self._y1) / 2 + self._y1
+        return Point(center_xcoord, center_ycoord)
+
+    def draw_move(self, to_cell, undo=False):
+        center_point = self.get_center_point()
+        to_cell_center_point = to_cell.get_center_point()
+        if undo == False:
+            self._win.draw_line(Line(center_point, to_cell_center_point), "red")
+        else:
+            self._win.draw_line(Line(center_point, to_cell_center_point), "gray")
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ def main():
     win = Window(800, 600)
 
     cell = Cell(win)
+    cell2 = Cell(win)
     cell.draw(200, 200, 250, 250)
+    cell2.draw(250, 200, 300, 250)
+    cell.draw_move(cell2)
 
     win.wait_for_close()
 
