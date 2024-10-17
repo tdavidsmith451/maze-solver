cd42107adfae676e84671b18cbe793ebeb9ab8c7
tdavidsmith451 <tdavidsmith@live.com>
2024-10-17 14:48:04 -0600

    added point and line classes to graphics.py as well as adding a draw line method to the window class

--- graphics.py
+++ graphics.py
@@ -3,22 +3,40 @@ from tkinter import Canvas, BOTH
 
 class Window:
     def __init__(self, width, height):
-        self.root = tk.Tk()
-        self.root.title("Maze Solver")
-        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.open = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root = tk.Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__open = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
-        self.open = True
-        while self.open == True:
+        self.__open = True
+        while self.__open == True:
             self.redraw()
         print("window closed...")
     
     def close(self):
-        self.open = False
+        self.__open = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
+
+    win.draw_line(Line(Point(2, 2), Point(200, 200)), "black")
+
     win.wait_for_close()
 
 main()
